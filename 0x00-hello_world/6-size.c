43dfe3bfacc6fe8c01ffa66cb8811cbe8d73133f
Beullah <samuelkayode2050@gmail.com>
2022-09-08 18:57:03 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - print the size of various types
- * Return: 0 if exited properly, non-zero otherwise
+ * main - Entry point
+ *
+ * Return (0)
  */
 int main(void)
 {

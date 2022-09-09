956e8b4f31d4fa620ff15c830b904a3857b054f9
Beullah <samuelkayode2050@gmail.com>
2022-09-08 17:17:25 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/** 
+ * main - print the pharse with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 id exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of an art,\n");
+	return (0);
+}

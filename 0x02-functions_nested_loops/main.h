69da541101ec3b09bb953b6cbb81e052eeeaca94
Beullah <samuelkayode2050@gmail.com>
2022-09-13 17:58:33 -0700

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,35 @@
+#ifndef HOLBERTON_H_INCLUDED
+
+#define HOLBERTON _H INCLUDED
+
+_putchar (char );
+
+void print_alphabet(void);
+
+void print_alphabet_x10 (void);
+
+int
+
+_islower (int c):
+
+int
+
+isalphat(int c)
+
+in Pabst anigncint m):
+
+int print last digit(int);
+
+void jack_bauer (void);
+
+int add(int a, int b);
+
+void print_times _table(int n);
+
+void num(intr.
+
+int m):
+
+void print_to_98 (int n):
+
+#endif

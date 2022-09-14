13317e69043dc11ea224e34b4133b0b9bfdaa28a
Beullah <samuelkayode2050@gmail.com>
2022-09-13 18:42:14 -0700

    Nain.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,35 +1,17 @@
-#ifndef HOLBERTON_H_INCLUDED
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#define HOLBERTON _H INCLUDED
-
-_putchar (char );
-
+int _putchar(char c);
 void print_alphabet(void);
-
-void print_alphabet_x10 (void);
-
-int
-
-_islower (int c):
-
-int
-
-isalphat(int c)
-
-in Pabst anigncint m):
-
-int print last digit(int);
-
-void jack_bauer (void);
-
-int add(int a, int b);
-
-void print_times _table(int n);
-
-void num(intr.
-
-int m):
-
-void print_to_98 (int n):
-
-#endif
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isaplha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+void add(int, int);
+void print_to_98(int n);
+void print_tines_tables(int n);
+#endif /* _MAIN_H_*/

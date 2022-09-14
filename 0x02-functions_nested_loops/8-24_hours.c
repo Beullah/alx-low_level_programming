632a3394ad37197e454944b739c70ecd3765715a
Beullah <samuelkayode2050@gmail.com>
2022-09-13 20:44:35 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,33 +8,26 @@
 void jack_bauer(void)
 
 {
+int i, j;
 
-	int i, j;
+for (i = 0; i <= 23; i++)
+{
+for (j = 0; j <= 59; j++)
 
+{
 
+_putchar(i % 10 + '0');
 
-	for (i = 0; i <= 23; i++)
+_putchar(i % 10 + '0');
 
-	{
+_putchar(':');
 
-		for (j = 0; j <= 59; j++)
+_putchar(j / 10 + '0');
 
-		{
+_putchar(j % 10 + '0');
 
-			_putchar(i % 10 + '0');
+_putchar('\n');
 
-			_putchar(i % 10 + '0');
-
-			_putchar(':');
-
-			_putchar(j / 10 + '0');
-
-			_putchar(j % 10 + '0');
-
-			_putchar('\n');
-
-		}
-
-	}
-
+}
+}
 }
